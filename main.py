f30355ef546f96e98739dd22466ceadbaaa12acf
Jonay Camara <jonaycp@gmail.com>
2019-03-12 09:24:09 -0700

    Splitted expenses and incomes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd 
 import matplotlib.pyplot as plt
+import numpy as np
 
 data = pd.read_csv(
     './data/data20190310.csv', 
@@ -11,22 +12,38 @@ data = pd.read_csv(
     usecols=['Date','Volume'] #, 'Note', 'Type']
     )
 
-print(data.shape) #Imprime las dimensiones del dataset
-print(data.count) #Imprime las dimensiones del dataset
+print("Data dimensions: ",data.shape) #Imprime las dimensiones del dataset
+#print(data.count) #Imprime las dimensiones del dataset
 col_names= data.columns.tolist() #Guardamos en una lista los nombres de las columnas
 print(col_names)
-x=data['Date']
-y=data['Volume']
+
 #print(data[data.Date > '01/31/2019']) #Imprime con filtro de fechas
 
 ###OJO porque las fechas estan como MM/DD/YYYY
 
 #print(data.describe()) #da cierta informacion de los valores numericos
 
+#HACEMOS UN LISTADO SOLO CON LOS GASTOS Y OTRO CON LOS INGRESOS
+gastos=data.loc[data['Volume'] < 0]
+ingresos=data.loc[data['Volume'] > 0]
 
+
+
+
+###SHOW THE PLOT
+x=gastos['Date']
+y=gastos['Volume']
 
 #First paint try
 fig = plt.figure()
 axes = fig.add_axes([0.1,0.1,0.8,0.8])
 axes.plot(x,y)
+
+print("Mean of average: ", np.average(y))
+print("Expense Min: ", np.amin(y))
+print("Expense Max: ", np.amax(y))
+print("Total Sum: ", np.sum(y))
+
 plt.show()
+
+
